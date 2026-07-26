9079374990f62797fb0c1fb129678aab49839190
Jason-Yeom <JasonYeom@protonmail.com>
2026-07-26 00:33:03 -0400

    Adding default features
    !ping
    !info

---
+++ index.js
@@ -0,0 +1,101 @@
+const { Client, GatewayIntentBits, Events, EmbedBuilder } = require('discord.js');
+require('dotenv').config();
+
+/*
+async function UserBasic() {
+	const rawResponseUserBasic = await fetch('https://api.torn.com/v2/user/basic', {
+		method: "GET",
+		headers: {
+			"cache-control": "no-store, no-cache, must-revalidate, max-age=0",
+			"content-type": "applications/json",
+			"Authorization": `ApiKey ${process.env.TORN_API}`
+		}
+	});
+
+	const UserBasicJson = await rawResponseUserBasic.json;
+
+	const playerTag = `${UserBasicJson.user.name} [${UserBasicJson.profile.id}]`;
+};
+*/
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
+	]
+});
+
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+	if (message.author.bot) return;
+	
+	if (message.content === "!ping") {
+		try { 
+			async function pingEdit() {
+				console.log("!ping detected!");
+				var timeCalled = await Date.now();
+				console.log(`got current time when !ping was called, ${timeCalled}`);
+				var sent = await message.reply("Pong!");
+				console.log(`sent the reply, "Pong!"`)
+				var timeSent = await Date.now();
+				console.log(`got the current time when Ping was sent, ${timeSent}`)
+				var delay = await timeSent - timeCalled;
+				console.log(`calculated the delay, ${delay}`);
+				await sent.edit(`Pong! ${delay} ms`)
+				console.log(`edited the message to; "Pong! ${delay} ms"`)
+			}
+			pingEdit();
+		} catch (err) {
+			var time = Date.now();
+			console.error(`Time editing for !ping ran at epoch ${time} failed, defaulted to fallback by just not editing.`)
+			return;
+		}
+	}
+
+	if (message.content === "!info") {
+		try {
+			async function wrapper(){
+			console.log(`!info detected`);
+			async function UserBasic() {
+				var rawResponseUserBasic = await fetch('https://api.torn.com/v2/user/basic', {
+					method: "GET",
+					headers: {
+						"cache-control": "no-store, no-cache, must-revalidate, max-age=0",
+						"content-type": "applications/json",
+						"Authorization": `ApiKey ${process.env.TORN_API}`
+					}
+				});
+				console.log(`fetching from torn, path is https://api.torn.com/v2/user/basic`);
+				var UserBasicJson = await rawResponseUserBasic.json();
+				var textJson = await JSON.stringify(UserBasicJson);
+				console.log(`converting it to json, result is ${textJson}`)
+				var playerTag = `${UserBasicJson.profile.name} [${UserBasicJson.profile.id}]`;
+				console.log(`player tag is ${playerTag}`);
+				return playerTag;
+			};
+			const playerTag = await UserBasic();
+
+			const embed = new EmbedBuilder()
+				.setTitle('Bot Information')
+				.setDescription(`This is a bot, where you can see ${playerTag}'s information.`)
+			message.reply({"embeds": [embed]});
+			};
+			wrapper();
+
+		} catch (err) {
+			console.error(`Error!: ${err}`);
+			const embed = new EmbedBuilder()
+				.setTitle("An Error Occured")
+				.setDescription(`Error: ${err}`)
+			message.reply({"embeds": [embed]});
+			return;
+		}
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);
